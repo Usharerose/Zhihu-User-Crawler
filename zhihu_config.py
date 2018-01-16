0dabea290139382eec7eb04baf2aaac8d480816f
Chaojie Yan <724378802@qq.com>
2018-01-16 15:33:43 +0800

    Create zhihu_config.py

---
+++ zhihu_config.py
@@ -0,0 +1,10 @@
+import re
+
+PROXY_SITES_LIST = [
+    'http://www.xicidaili.com/nn/',
+    'https://www.kuaidaili.com/free/inha',
+    'http://www.proxylists.net/?HTTP',
+    #'https://proxy.mimvp.com/free.php?proxy=in_hp' 端口号以图片展示
+]
+
+PROXY_REGU = re.compile(pattern='[0-9]+(?:\.[0-9]+){3}:\d{2,4}')
