9487b76e9dbd4eb7373ff6afcaac1689bc66114d
Chaojie Yan <724378802@qq.com>
2018-01-16 16:55:39 +0800

    Update zhihu_engine.py

--- zhihu_engine.py
+++ zhihu_engine.py
@@ -1,33 +1,41 @@
-import zhihu_crawler, zhihu_bloomfilter
-import time, queue, os
-from multiprocessing.dummy import Pool
+import zhihu_crawler, zhihu_bloom
+import time, queue, os, threading
 
-q = queue.Queue()
 crawler = zhihu_crawler.Zhihu_Crawler()
-bf = zhihu_bloomfilter.BloomFilter()
+bf = zhihu_bloom.BloomFilter()
 
-def starter(inputurl):
-    url_list = crawler.parse_user_profile(url=inputurl)
-    for x in url_list:
-        q.put(x)
-    print('ok')
+def starter(inputurl, queue):
+    if not bf.contains(inputurl):
+        bf.add(inputurl)
+        url_list = crawler.parse_user_profile(url=inputurl)
+        for x in url_list:
+            queue.put(x)
+        print('ok')
+    else:
+        print('This has exited, which will be ignored')
 
-def worker():
+def worker(queue):
     while True:
-        url = q.get()
+        url = queue.get()
         url_list = crawler.parse_user_profile(url=url)
         for x in url_list:
-            if not bf.contain(x):
-                q.put(x)
+            if not bf.contains(x):
+                queue.put(x)
                 bf.add(x)
             else:
-                print('%s has existed, which will be ignored.' %x)
+                print('This has exited, which will be ignored')
+        print('ok')
 
 if __name__ == "__main__":
-    initial_url = '请在这里输入您的爬取种子网页，如https://www.zhihu.com/people/XXXX'
-    bf.add(initial_url)
-    starter(initial_url)
-    pool = Pool(os.cpu_count())
-    task = pool.apply_async(func=worker)
-    pool.close()
-    pool.join()
+
+    # the start page
+    q = queue.Queue()
+    initial_url = 'https://www.zhihu.com/people/sgai'
+    starter(initial_url, q)
+
+    for x in range(os.cpu_count()*3):
+        task = threading.Thread(target=worker, args=(q,))
+        task.setDaemon(True)
+        task.start()
+
+    q.join()
